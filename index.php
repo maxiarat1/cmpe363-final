6f8477b4c044f6ba75c3a55f0e6a35d96c92478d
maxiarat1 <71734421+maxiarat1@users.noreply.github.com>
2023-01-12 16:11:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   <head>
     <meta charset="utf-8">
     <title>CMPE-363</title>
-    <script src="js/main.js" type="module"></script>
+    <!--<script src="js/main.js" type="module"></script>-->
   </head>
   <body>
     
