899e4d363305921e54dea737eacfd134754e83cd
maxiarat1 <71734421+maxiarat1@users.noreply.github.com>
2023-01-12 16:10:44 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>CMPE-363</title>
+    <script src="js/main.js" type="module"></script>
+  </head>
+  <body>
+    
+    Is this working?
+  </body>
+</html>
